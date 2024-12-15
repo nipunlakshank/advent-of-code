a937b3eb1104386fe10c841f0d8796a073352a03
Nipun Lakshan <nipunlakshankumara@gmail.com>
2024-12-15 21:53:26 +0530

    part 1, day 1, 2024

---
+++ 2024/Day 1/part1.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# start time in milliseconds
+start=$(gdate +%s.%3N)
+
+input="$(cat input1.txt)"
+
+distance=0
+list1=()
+list2=()
+
+i=0
+for locationId in $input; do
+    if [ $((i % 2)) -eq 0 ]; then
+        list1+=($locationId)
+    else
+        list2+=($locationId)
+    fi
+    i=$((i + 1))
+done
+
+sortList1=($(for i in "${list1[@]}"; do echo $i; done | sort -n))
+sortList2=($(for i in "${list2[@]}"; do echo $i; done | sort -n))
+
+for i in "${!sortList1[@]}"; do
+    a=${sortList1[$i]}
+    b=${sortList2[$i]}
+
+    if [ $((a - b)) -gt 0 ]; then
+        distance=$((distance + a - b))
+    else
+        distance=$((distance + b - a))
+    fi
+done
+
+# end time in milliseconds
+end=$(gdate +%s.%3N)
+
+echo $distance
+echo "Execution time: $(echo "$end - $start" | bc) seconds"
